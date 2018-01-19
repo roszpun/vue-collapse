fef10a974c57468db4cfd822cd41f578db11abb2
roszpun <szerszen.daniel@gmail.com>
2018-01-19 12:24:51 +0100

    Initial version of demo

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  #app
+    v-collapse-group
+      v-collapse-wrapper
+        article.message.is-warning
+          .message-header(v-collapse-toggle="")
+            p Warning
+            button.delete(aria-label='delete')
+          .message-body(v-collapse-content='')
+            |Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 500px;
+  margin: 150px auto 0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
